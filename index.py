eb3dbc85bef28aa09b0927a6a74a3b58a5a7e005
Sanket <sanketkatkade782@gmail.com>
2023-04-11 16:43:03 +0530

    Modified code to show meal data properly

--- index.py
+++ index.py
@@ -18,9 +18,24 @@ if response.status_code == 200:
         dish_name = meal['strMeal']
         recipe = meal['strInstructions']
         
-        print("Meal Data :", meal)
-        print("Dish Name :", dish_name)
-        print("Recipe :", recipe)
+        ingredients = []
+        for i in range(1, 21):
+            ingredient_key = f'strIngredient{i}'
+            measure_key = f'strMeasure{i}'
+            ingredient = meal.get(ingredient_key, None)
+            measure = meal.get(measure_key, None)
+            if ingredient is not None and measure is not None:
+                ingredient = ingredient.strip()
+                measure = measure.strip()
+                if ingredient:
+                    ingredient_with_measure = f"{measure} {ingredient}"
+                    ingredients.append(ingredient_with_measure)
+
+        print(f"Dish Name: {dish_name}")
+        print(f"Recipe: {recipe}")
+        print("Ingredients:")
+        for ingredient in ingredients:
+            print(ingredient)
     else:
         print(f"No meals found for '{dish_name_input}'.")
 else:
